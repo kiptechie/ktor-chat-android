d667313d397235f7cfe13de34cfa62607c9f7582
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-11-26 21:21:16 +0300

    added message dto
    Signed-off-by: Timothy Kipkorir Serem <theecodepoet@gmail.com>

--- app/src/main/java/com/kiptechie/ktor_chat_android/data/remote/dto/MessageDto.kt
+++ app/src/main/java/com/kiptechie/ktor_chat_android/data/remote/dto/MessageDto.kt
@@ -1,5 +1,26 @@
 package com.kiptechie.ktor_chat_android.data.remote.dto
 
+import com.kiptechie.ktor_chat_android.domain.model.Message
+import kotlinx.serialization.Serializable
+import java.text.DateFormat
+import java.util.*
+
+@Serializable
 data class MessageDto(
-    //
-)
+    val text: String,
+    val timestamp: Long,
+    val username: String,
+    val id: String
+) {
+    fun toMessage(): Message {
+        val date = Date(timestamp)
+        val formattedDate = DateFormat
+            .getDateInstance(DateFormat.DEFAULT)
+            .format(date)
+        return Message(
+            text = text,
+            formattedTime = formattedDate,
+            username = username
+        )
+    }
+}
