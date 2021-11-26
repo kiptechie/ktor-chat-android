e991235b5c5be015776a97703b71a567d6dda7dc
Timothy Kipkorir Serem <theecodepoet@gmail.com>
2021-11-26 21:16:56 +0300

    added chat socket service

---
+++ app/src/main/java/com/kiptechie/ktor_chat_android/data/remote/socket/ChatSocketService.kt
@@ -0,0 +1,28 @@
+package com.kiptechie.ktor_chat_android.data.remote.socket
+
+import com.kiptechie.ktor_chat_android.core.util.Resource
+import com.kiptechie.ktor_chat_android.domain.model.Message
+import kotlinx.coroutines.flow.Flow
+
+interface ChatSocketService {
+
+    companion object {
+        const val BASE_URL = "ws://192.168.100.14:8080"
+    }
+
+    sealed class Endpoints(val url: String) {
+        object ChatSocket : Endpoints("$BASE_URL/chat-socket")
+    }
+
+    suspend fun initSession(
+        userName: String
+    ): Resource<Unit>
+
+    suspend fun sendMessage(
+        message: String
+    )
+
+    fun observeMessages(): Flow<Message>
+
+    suspend fun closeSession()
+}
